8c766bc147ad7833f25f05d6710812344ec57d28
Chelsea Long <clong6@gmail.com>
2019-04-15 22:34:31 -0600

    auth0

---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+  domain: "attendus.auth0.com",
+  clientId: "LJldyxsZrlGRo5CE1SnMHMOXZ6ry2xBc",
+  callbackUrl: "http://localhost:3000/callback"
+};
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,20 +1,37 @@
 import React from "react";
 import "./style.css";
 
-function Nav() {
-    return (
+function Nav(props) {
+  let login = <a onClick={props.auth.login}>Log In</a>;
+  if (props.auth.isAuthenticated()) {
+    login = <a onClick={props.auth.logout}>Log Out</a>;
+  }
+  return (
     <nav>
-        <div className="nav-wrapper">
-            <a href="/" className="brand-logo">Attend-US</a>
-            <ul id="nav-mobile" className="right">
-                <li><a href="/roster">Roster</a></li>
-                <li><a href="/member">New Member</a></li>
-                <li><a href="/event">New Event</a></li>
-                <li><a href="/history">Event History</a></li>
-            </ul>
-        </div>
+      <div className="nav-wrapper">
+        <a href="/" className="brand-logo">
+          Attend-US
+        </a>
+        <ul id="nav-mobile" className="right">
+          <li>
+            <a href="/roster">Roster</a>
+          </li>
+          <li>
+            <a href="/member">New Member</a>
+          </li>
+          <li>
+            <a href="/event">New Event</a>
+          </li>
+          <li>
+            <a href="/history">Event History</a>
+          </li>
+          <li>
+            {login}
+          </li>
+        </ul>
+      </div>
     </nav>
-    )
+  );
 }
 
 export default Nav;
