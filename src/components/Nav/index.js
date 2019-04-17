f38b6515122dd73ae2d743383c8f9ee73c5b0024
sethbaldridge87 <sethbaldridge87@gmail.com>
2019-04-16 20:03:39 -0600

    Mobile menu added

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,35 +3,36 @@ import { Link } from "react-router-dom";
 import "./style.css";
 
 function Nav(props) {
+  // eslint-disable-next-line
   let login = <a onClick={props.auth.login}>Log In</a>;
   if (props.auth.isAuthenticated()) {
+    // eslint-disable-next-line
     login = <a onClick={props.auth.logout}>Log Out</a>;
   }
+  
   return (
-    <nav>
-      <div className="nav-wrapper">
-        <Link to="/" className="brand-logo">
-          Attend-US
-        </Link>
-        <ul id="nav-mobile" className="right">
-          <li>
-            <Link to="/roster">Roster</Link>
-          </li>
-          <li>
-            <Link to="/member">New Member</Link>
-          </li>
-          <li>
-            <Link to="/event">New Event</Link>
-          </li>
-          <li>
-            <Link to="/history">Event History</Link>
-          </li>
-          <li>
-            {login}
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div>
+      <nav>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo">Attend-US</Link>
+          <Link to="/" data-target="mobile-demo" className="sidenav-trigger"><i class="material-icons">menu</i></Link>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><Link to="/roster">Roster</Link></li>
+            <li><Link to="/member">New Member</Link></li>
+            <li><Link to="/event">New Event</Link></li>
+            <li><Link to="/history">Event History</Link></li>
+            <li>{login}</li>
+          </ul>
+        </div>
+      </nav>
+      <ul class="sidenav" id="mobile-demo">
+        <li><Link to="/roster">Roster</Link></li>
+        <li><Link to="/member">New Member</Link></li>
+        <li><Link to="/event">New Event</Link></li>
+        <li><Link to="/history">Event History</Link></li>
+        <li id="login_button">{login}</li>
+      </ul>
+    </div>
   );
 }
 
