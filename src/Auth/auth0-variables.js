b858a59a779a065e1aefe090c5c5fcabe1d8080e
Scott171 <K.Scott.Hewitson@gmail.com>
2019-04-16 19:58:58 -0600

    initial back end

--- src/Auth/auth0-variables.js
+++
@@ -1,8 +0,0 @@
-var url = window.location.href;
-var urlParts = url.split("/");
-
-export const AUTH_CONFIG = {
-  domain: "attendus.auth0.com",
-  clientId: "LJldyxsZrlGRo5CE1SnMHMOXZ6ry2xBc",
-  callbackUrl: urlParts[0] + "//" + urlParts[2] + "/callback"
-};
