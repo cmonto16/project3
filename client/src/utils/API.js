eebc117adda26d561b6cc8eb7c9c91c2730bc5bd
Scott171 <K.Scott.Hewitson@gmail.com>
2019-04-20 12:08:08 -0600

    linked history page with backend

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -16,5 +16,21 @@ export default {
   // Saves a user to the database
   saveUsers: function(userData) {
     return axios.post("/api/users", userData);
+  },
+   // Gets all events
+   getEvents: function() {
+    return axios.get("/api/events");
+  },
+  // Gets the event with the given id
+  getEvent: function(id) {
+    return axios.get("/api/events/" + id);
+  },
+  // Deletes the event with the given id
+  deleteEvents: function(id) {
+    return axios.delete("/api/events/" + id);
+  },
+  // Saves an event to the database
+  saveEvents: function(eventData) {
+    return axios.post("/api/events", eventData);
   }
 };
