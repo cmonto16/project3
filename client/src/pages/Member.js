8aa7bcd520959ef209fca64468d5afa7438cdb3e
Chelsea Long <clong6@gmail.com>
2019-04-21 23:19:44 -0600

    New member and event auth0 validation

--- client/src/pages/Member.js
+++ client/src/pages/Member.js
@@ -1,73 +1,131 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Member extends Component {
-    constructor () {
-        super();
-        this.state = {
-            firstName : '',
-            lastName : '',
-            member_number : '',
-            data_approved : '',
-            nickname: '',
-            image_link: ''
-        };
-    this.inputChange = this.inputChange.bind(this);
-    }
-    inputChange (event) {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
+  constructor() {
+    super();
+    this.state = {
+      form: {
+        first_name: "",
+        last_name: "",
+        member_number: 0,
+        approval_date: null,
+        nickname: "",
+        hours: 0,
+        image: ""
+      }
     };
-    render() {
-        
-        return (
-            <div className="container">
-                <div className="row">
-                    <h1>Input new member data here</h1>
-                    <form className="col s12">
-                        <div className="row">
-                            <div className="col s6 input-field">
-                                <label htmlFor="fname">First Name</label>
-                                <input className="" type="text" name="firstName" id="fname" onChange={this.inputChange}/>
-                            </div>
-                            <div className="col s6 input-field">
-                                <label htmlFor="lname">Last Name</label>
-                                <input className="" type="text" name="lastName" id="lname" onChange={this.inputChange} />
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col s6 input-field">
-                                <label htmlFor="member_num">Member#</label>
-                                <input className="" type="number" name="member_number" id="member_num" onChange={this.inputChange} />
-                            </div>
-                            <div className="col s6 input-field">
-                                <label htmlFor="member_date">Date Approved</label>
-                                <input className="" type="text" name="data_approved" id="member_date" onChange={this.inputChange} />
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col s6 input-field">
-                                <label htmlFor="nickname">Nickname</label>
-                                <input className="" type="text" name="nickname" id="nickname" onChange={this.inputChange} />
-                            </div>
-                            <div className="col s6 input-field">
-                                <label htmlFor="image_link">Link to Profile Image</label>
-                                <input className="" type="text" name="image_link" id="image_link" onChange={this.inputChange} />
-                            </div>
-                        </div>
-                        <div className="row">
-                            <button className="btn waves-effect waves-light green" type="submit">Submit
-                                <i className="material-icons right">send</i>
-                            </button>
-                            <button className="btn waves-effect waves-light red" type="reset">Reset
-                                <i className="material-icons right">undo</i>
-                            </button>
-                        </div>
-                    </form>
-                </div>
+    this.inputChange = this.inputChange.bind(this);
+  }
+  createMember() {
+    const { getAccessToken } = this.props.auth;
+    const headers = { Authorization: `Bearer ${getAccessToken()}` };
+    this.state.form.member_number = parseInt(this.state.form.member_number);
+    axios
+      .post(`/api/users`, this.state.form, { headers })
+      .then(response => this.setState({ message: "Success!" }))
+      .catch(error =>
+        this.setState({ message: error.message || "Failed to save." })
+      );
+  }
+
+  inputChange(event) {
+    this.setState({
+      form: { ...this.state.form, [event.target.name]: event.target.value }
+    });
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          {this.state.message && <h2>{this.state.message}</h2>}
+          <h1>Input new member data here</h1>
+          <form className="col s12">
+            <div className="row">
+              <div className="col s6 input-field">
+                <label htmlFor="fname">First Name</label>
+                <input
+                  className=""
+                  type="text"
+                  name="first_name"
+                  id="fname"
+                  onChange={this.inputChange}
+                />
+              </div>
+              <div className="col s6 input-field">
+                <label htmlFor="lname">Last Name</label>
+                <input
+                  className=""
+                  type="text"
+                  name="last_name"
+                  id="lname"
+                  onChange={this.inputChange}
+                />
+              </div>
             </div>
-        )
-    }
+            <div className="row">
+              <div className="col s6 input-field">
+                <label htmlFor="member_num">Member#</label>
+                <input
+                  className=""
+                  type="number"
+                  name="member_number"
+                  id="member_num"
+                  onChange={this.inputChange}
+                />
+              </div>
+              <div className="col s6 input-field">
+                <label htmlFor="member_date">Date Approved</label>
+                <input
+                  className=""
+                  type="text"
+                  name="approval_date"
+                  id="member_date"
+                  onChange={this.inputChange}
+                />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col s6 input-field">
+                <label htmlFor="nickname">Nickname</label>
+                <input
+                  className=""
+                  type="text"
+                  name="nickname"
+                  id="nickname"
+                  onChange={this.inputChange}
+                />
+              </div>
+              <div className="col s6 input-field">
+                <label htmlFor="image_link">Link to Profile Image</label>
+                <input
+                  className=""
+                  type="text"
+                  name="image"
+                  id="image_link"
+                  onChange={this.inputChange}
+                />
+              </div>
+            </div>
+            <div className="row">
+              <button
+                className="btn waves-effect waves-light green"
+                onClick={this.createMember.bind(this)}
+                type="button"
+              >
+                Submit
+                <i className="material-icons right">send</i>
+              </button>
+              <button className="btn waves-effect waves-light red" type="reset">
+                Reset
+                <i className="material-icons right">undo</i>
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Member;
--- routes/api/events.js
+++ routes/api/events.js
@@ -1,16 +1,17 @@
 const router = require("express").Router();
 const eventsController = require("../../controllers/eventsController");
+const validateToken = require("../../middleware/validateToken");
 
 // Matches with "/api/events"
 router.route("/")
   .get(eventsController.findAll)
-  .post(eventsController.create);
+  .post(validateToken, eventsController.create);
 
 // Matches with "/api/events/:id"
 router
   .route("/:id")
   .get(eventsController.findById)
-  .put(eventsController.update)
+  .put(validateToken, eventsController.update)
   .delete(eventsController.remove);
 
 module.exports = router;
